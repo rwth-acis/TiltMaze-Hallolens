06f0b83a32e307578cba3b108b8bd0dc0b533325
stevenz <stevenzambr@gmail.com>
2019-11-17 16:49:09 +0100

    Aufgabe 2.2c Done
    - implemented focus highlight feature (script)
    - added the script to handle and 3 doors

---
+++ Uebung02/Assets/TiltMaze/Scripts/FocusHighlight.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class FocusHighlight : MonoBehaviour, IFocusable {
+
+	// public color variables for editing in inspector
+	public Color defaultColor = Color.green;
+	public Color highlightColor = Color.red;
+
+	private Renderer rend;
+	private bool inside = false;
+
+	// setting default color to object when starting game
+	void Start () {
+		defaultColor = Color.green;
+		rend = gameObject.GetComponent<Renderer>();
+		rend.material.color = defaultColor;
+	}
+
+	// used to be able to change colors with inspector while in game
+	void Update () {
+		if (inside == false){
+			rend.material.color = defaultColor;
+		}
+	}
+
+	// highlight color when gazing at object
+ 	public void OnFocusEnter(){
+	    rend.material.color = highlightColor;
+	    inside = true;
+	}
+
+	// default color when not gazing at object
+	public void OnFocusExit(){
+	    rend.material.color = defaultColor;
+	    inside = false;
+	}
+}
