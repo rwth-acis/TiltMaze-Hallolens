acedf57ca42633d9c44afa2eb98a23300fcd1fc5
stevenz <stevenzambr@gmail.com>
2019-11-19 14:02:29 +0100

    Aufgabe 2.2d Works
    - implemented mazeMover script
    - works as intented but not as asked by the assignment description

---
+++ Uebung02/Assets/TiltMaze/Scripts/MazeMover.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using IManipulationHandler = HoloToolkit.Unity.InputModule.IManipulationHandler;
+
+public class MazeMover : MonoBehaviour, IManipulationHandler {
+
+	private Vector3 startPosition;
+    private Vector3 targetPosition;
+    private Vector3 mazeRotation;
+
+	void Start () {
+        startPosition = gameObject.transform.position;
+    }
+
+	void Update () {
+		
+	}
+
+    public void OnManipulationUpdated(ManipulationEventData eventData){
+        targetPosition = eventData.CumulativeDelta * 100;
+        gameObject.transform.parent.transform.rotation = Quaternion.Euler(mazeRotation.x + targetPosition.y, mazeRotation.y, mazeRotation.z - targetPosition.x);
+    }
+
+    public void OnManipulationCanceled(ManipulationEventData eventData){
+
+    }
+
+    public void OnManipulationCompleted(ManipulationEventData eventData){
+        mazeRotation = gameObject.transform.parent.transform.rotation.eulerAngles;
+    }
+
+    public void OnManipulationStarted(ManipulationEventData eventData){
+        mazeRotation = gameObject.transform.parent.transform.rotation.eulerAngles;
+    }
+}
