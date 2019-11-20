69b97a2c9d7d99816ff9d77aadf5428a3a436872
stevenz <stevenzambr@gmail.com>
2019-11-20 16:44:47 +0100

    Aufgabe 2.2e Done
    - Added overrideFocusedObject feature to mazeMover script

--- Uebung02/Assets/TiltMaze/Scripts/MazeMover.cs
+++ Uebung02/Assets/TiltMaze/Scripts/MazeMover.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using HoloToolkit.Unity.InputModule;
 using IManipulationHandler = HoloToolkit.Unity.InputModule.IManipulationHandler;
+using InputManager = HoloToolkit.Unity.InputModule.InputManager;
 
-public class MazeMover : MonoBehaviour, IManipulationHandler {
+public class MazeMover : MonoBehaviour, IManipulationHandler{
 
 	private Vector3 startPosition;
     private Vector3 targetPosition;
@@ -19,19 +20,22 @@ public class MazeMover : MonoBehaviour, IManipulationHandler {
 	}
 
     public void OnManipulationUpdated(ManipulationEventData eventData){
+        InputManager.Instance.OverrideFocusedObject = gameObject;
         targetPosition = eventData.CumulativeDelta * 100;
         gameObject.transform.parent.transform.rotation = Quaternion.Euler(mazeRotation.x + targetPosition.y, mazeRotation.y, mazeRotation.z - targetPosition.x);
     }
 
     public void OnManipulationCanceled(ManipulationEventData eventData){
-
+        InputManager.Instance.OverrideFocusedObject = null;
     }
 
     public void OnManipulationCompleted(ManipulationEventData eventData){
         mazeRotation = gameObject.transform.parent.transform.rotation.eulerAngles;
+        InputManager.Instance.OverrideFocusedObject = null;
     }
 
     public void OnManipulationStarted(ManipulationEventData eventData){
+        InputManager.Instance.OverrideFocusedObject = gameObject;
         mazeRotation = gameObject.transform.parent.transform.rotation.eulerAngles;
     }
 }
