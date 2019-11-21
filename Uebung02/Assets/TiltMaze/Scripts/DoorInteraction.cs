77fdb6e583417ab10a7c45fc0d53aadaab77ce7c
danielbekhter <57594319+danielbekhter@users.noreply.github.com>
2019-11-21 23:34:35 +0100

    Die Türe öffnen und schließen sich jetzt, wenn man sie anklickt.
    Dies wurde im Script "DoorInteraction" implementiert.

---
+++ Uebung02/Assets/TiltMaze/Scripts/DoorInteraction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+using IInputClickHandler = HoloToolkit.Unity.InputModule.IInputClickHandler;
+
+public class DoorInteraction : MonoBehaviour, IInputClickHandler {
+
+    private DoorController contr;
+    private bool state = false;
+
+    // Use this for initialization
+    void Start () {
+        contr = gameObject.GetComponent<DoorController>();
+        contr.DoorOpen = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        if(state == false)
+        {
+            contr.DoorOpen = true;
+            state = true;
+        }
+        else
+        {
+            contr.DoorOpen = false;
+            state = false;
+        }
+    }
+
+}
